c19563114c0491c4bb30113b98446abe90a80b1e
maysazu4 <77391084+maysazu4@users.noreply.github.com>
2024-05-26 22:44:22 +0300

    insert data to data base

--- Queries/insert_data.py
+++ Queries/insert_data.py
@@ -3,13 +3,18 @@ import database
 import random
 
 
+'''
+Inserts a single type to the types table
+'''
 def insert_single_type(connection, type_name):
     database.execute_query(connection,
         "INSERT INTO Type (name) VALUES (%s) ON DUPLICATE KEY UPDATE name=name",
         (type_name,)
     )
     return connection.cursor().lastrowid
-
+'''
+Inserts single pokemon to the pokemen table and its type to the pokemenType table 
+'''
 def insert_pokemon(connection, pokemon, type_name):
     print(type_name)
     database.execute_query(connection,
@@ -20,55 +25,63 @@ def insert_pokemon(connection, pokemon, type_name):
         "INSERT IGNORE INTO pokemonType (pokemon_id, type_name) VALUES (%s, %s)",
         (pokemon["id"], type_name)
     )
+'''
+Insert data into the pokemon table
+'''
+def insert_into_pokemons_and_types(data, connection):
+    for pokemon in data:
+        # If type is a single string
+        if isinstance(pokemon["type"], str):
+            insert_single_type(connection, pokemon["type"])
+            insert_pokemon(connection, pokemon, pokemon["type"])
+        # If type is a list of strings
+        elif isinstance(pokemon["type"], list):
+            for type_name in pokemon["type"]:
+                insert_single_type(connection, type_name)
+                insert_pokemon(connection, pokemon, type_name)
+'''
+Insert data into the trainer table
+'''
+def insert_into_trainer(data, connection):
+    trainers = set()
+    for pokemon in data:
+        for trainer in pokemon["ownedBy"]:
+            trainers.add((trainer["name"], trainer["town"]))
 
-# Load JSON data
-with open('DB/pokemons_data.json') as file:
-    data = json.load(file)
-
-# Connect to MySQL database
-connection = database.connect_to_database()
-
-# Insert data into the pokemon table
-    # Insert data into the pokemon table
-for pokemon in data:
-    # If type is a single string
-    if isinstance(pokemon["type"], str):
-        insert_single_type(connection, pokemon["type"])
-        insert_pokemon(connection, pokemon, pokemon["type"])
-    # If type is a list of strings
-    elif isinstance(pokemon["type"], list):
-        for type_name in pokemon["type"]:
-            insert_single_type(connection, type_name)
-            insert_pokemon(connection, pokemon, type_name)
-
-
-
-
-
-# Insert data into the trainer table
-trainers = set()
-for pokemon in data:
-    for trainer in pokemon["ownedBy"]:
-        trainers.add((trainer["name"], trainer["town"]))
-
-for trainer in trainers:
-    database.execute_query(connection,
-        "INSERT IGNORE INTO trainer (name, town) VALUES (%s, %s)",
-        trainer
-    )
-
-# Insert data into the ownership table
-for pokemon in data:
-    for trainer in pokemon["ownedBy"]:
-        if isinstance(pokemon["type"], list): 
-            type_name = random.choice(pokemon["type"])
-        else:
-            type_name = pokemon["type"]
-
+    for trainer in trainers:
         database.execute_query(connection,
-            "INSERT IGNORE INTO ownership (trainer_name, pokemon_id, type_name) VALUES (%s, %s, %s)",
-            (trainer["name"], pokemon["id"], type_name)
+            "INSERT IGNORE INTO trainer (name, town) VALUES (%s, %s)",
+            trainer
         )
+'''
+Insert data into the ownership table
+'''
+def insert_into_ownership(data, connection):
+    for pokemon in data:
+        for trainer in pokemon["ownedBy"]:
+            if isinstance(pokemon["type"], list): 
+                type_name = random.choice(pokemon["type"])
+            else:
+                type_name = pokemon["type"]
 
-# Commit changes and close connection
-database.close_connection(connection)
+            database.execute_query(connection,
+                "INSERT IGNORE INTO ownership (trainer_name, pokemon_id, type_name) VALUES (%s, %s, %s)",
+                (trainer["name"], pokemon["id"], type_name)
+            )
+'''
+Inserts all the data in the json files to MySql database
+'''
+def insert_data():
+    # Load JSON data
+    with open('DB/pokemons_data.json') as file:
+        data = json.load(file)
+
+    # Connect to MySQL database
+    connection = database.connect_to_database()
+
+    insert_into_pokemons_and_types(data,connection)
+    insert_into_trainer(data,connection)
+    insert_into_ownership(data, connection)
+
+    # Commit changes and close connection
+    database.close_connection(connection)
