79a35fef63385d913a4436cc79a179379e74e1c0
maysazu4 <77391084+maysazu4@users.noreply.github.com>
2024-05-31 16:42:46 +0300

    adding docstring

--- pokemon_api/routes/evolve_router.py
+++ pokemon_api/routes/evolve_router.py
@@ -1,31 +1,45 @@
 from fastapi import APIRouter, HTTPException
-from Queries import query_exist
-from Queries.query_delete_pokemon import delete_pokemon_of_trainer
-from Queries.query_insert_pokemon import insert_pokemon
-from Queries.query_insert_pokemon_to_trainer import insert_into_ownership
+from Queries import trainer, pokemon
 from pokemon_api.utils.evolve_utils import get_evolved_pokemon
 from pokemon_api.utils.get_pokemon_info_utils import get_pokemon_info
 
 router = APIRouter()
 
 
+"""
+ Evolve a Pokémon for a specific trainer.
+ This endpoint checks if the given trainer and Pokémon exist, and if the trainer owns the Pokémon.
+ If the Pokémon can evolve, it will be evolved and updated in the trainer's list.
+ Parameters:
+ - trainer_name: str - The name of the trainer.
+ - pokemon_name: str - The name of the Pokémon to be evolved.
+ Raises:
+ - HTTPException: 404 if the trainer does not exist.
+ - HTTPException: 404 if the Pokémon does not exist.
+ - HTTPException: 404 if the trainer does not own the specified Pokémon.
+ - HTTPException: 400 if the Pokémon cannot evolve.
+ - HTTPException: 409 if the trainer already owns the evolved Pokémon.
+ Returns:
+ - dict: A message indicating the successful evolution of the Pokémon.
+ """
+
 @router.patch("/trainers/{trainer_name}/pokemons/{pokemon_name}")
 async def evolve_pokemon(trainer_name: str, pokemon_name: str):
-    if not query_exist.trainer_exists(trainer_name):
+    if not trainer.trainer_exists(trainer_name):
         raise HTTPException(status_code=404, detail=f"{trainer_name} trainer not found.")
-    if not query_exist.pokemon_exists(pokemon_name):
+    if not pokemon.pokemon_exists(pokemon_name):
         raise HTTPException(status_code=404, detail=f"{pokemon_name} pokemon not found.")
-    if not query_exist.trainer_has_pokemon(trainer_name, pokemon_name):
+    if not trainer.trainer_has_pokemon(trainer_name, pokemon_name):
         raise HTTPException(status_code=404, detail=f"{trainer_name} does not have {pokemon_name} pokemon")
     evolved_pokemon_name = await get_evolved_pokemon(pokemon_name)
     if not evolved_pokemon_name:
         raise HTTPException(status_code=400, detail=f"{pokemon_name} does not evolve")
-    if query_exist.trainer_has_pokemon(trainer_name, evolved_pokemon_name):
+    if trainer.trainer_has_pokemon(trainer_name, evolved_pokemon_name):
         raise HTTPException(status_code=409, detail=f"{trainer_name} already has {evolved_pokemon_name} pokemon")
-    delete_pokemon_of_trainer(trainer_name, pokemon_name)
-    insert_into_ownership(evolved_pokemon_name, trainer_name)
-    if not query_exist.pokemon_exists(evolved_pokemon_name):
+    pokemon.delete_pokemon_of_trainer(trainer_name, pokemon_name)
+    trainer.insert_into_ownership(evolved_pokemon_name, trainer_name)
+    if not pokemon.pokemon_exists(evolved_pokemon_name):
         pokemon_info = get_pokemon_info(pokemon_name)
-        insert_pokemon(pokemon_info)
+        pokemon.insert_pokemon(pokemon_info)
 
     return {"message": f"Evolved {pokemon_name} to {evolved_pokemon_name} for trainer {trainer_name}"}
