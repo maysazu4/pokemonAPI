88890bdc8a92f6caeee2c14ed69e2d5326dcfd6f
maysazu4 <77391084+maysazu4@users.noreply.github.com>
2024-05-31 16:42:14 +0300

    test add pokemon

--- tests/test_pokemon_router.py
+++ tests/test_pokemon_router.py
@@ -13,10 +13,9 @@ def test_get_pokemons_type():
     assert response.json() == ["charmander","charmeleon","charizard","vulpix","ninetales","growlithe","arcanine","ponyta","rapidash","magmar","flareon","moltres"]
 
 def test_get_pokemons_trainer_name():
-    response = client.get("/pokemons?trainer_name=Giovanni")
+    response = client.get("/pokemons?trainer_name=Drasna")
     assert response.status_code == 200
-    assert response.json() == ["bulbasaur","venusaur","charmander","wartortle","caterpie","beedrill","oddish","parasect","primeape","weepinbell","eevee"]
-
+    assert response.json() == ["wartortle", "caterpie", "beedrill", "arbok", "clefairy", "wigglytuff", "persian", "growlithe", "machamp", "golem", "dodrio", "hypno", "cubone", "eevee", "kabutops"]
 def test_get_pokemons_both_params():
     
     response = client.get("/pokemons?type=Electric&trainer_name=Ash")
@@ -27,3 +26,17 @@ def test_get_pokemons_no_params():
     response = client.get("/pokemons")
     assert response.status_code == 400
     assert response.json() == {"detail": "Specify at least one query parameter: type or trainer_name."}
+
+def test_add_pokemon_success():
+    response = client.post("/pokemons?pokemon_name=yanma")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Pokemon added successfully"}
+    response = client.get(("/pokemons?type=bug"))
+    result  = response.json()
+    assert "yanma" in result
+    response = client.get(("/pokemons?type=flying"))
+    result = response.json()
+    assert "yanma" in result
+
+
+
